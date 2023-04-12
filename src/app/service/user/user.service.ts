f3176eed4a232ce59b56a53adbe8b59811b76d07
Guts 6667 <chambet.dev@gmail.com>
2023-04-12 10:04:35 +0200

    End first part

--- src/app/service/user/user.service.ts
+++ src/app/service/user/user.service.ts
@@ -1,40 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor() {}
-  public users: string[] = [
-    'AquaExplorer12',
-    'SunriseSeeker87',
-    'StellarAdventurer45',
-    'MoonlitMarauder39',
-    'CelestialVoyage23',
-    'GalaxyGazer64',
-    'CosmicChampion81',
-    'NebulaNavigator07',
-    'SkyboundTrailblazer31',
-    'StarstruckPioneer94',
-    'EternalVoyager53',
-    'AstroWanderer28',
-  ];
+  public users: User[] = [];
 
-  public addUser(user: string) {
-    this.users.push(user);
-  }
-  public removeUser(index: number) {
-    if (this.users[index]) {
-      this.users.splice(index, 1);
-    }
+  constructor(private httpClient: HttpClient) {}
+
+  public getUsers() {
+    this.httpClient.get<User[]>('http://localhost:3000/users').subscribe((users: User[]) => {
+      this.users = users;
+    });
   }
 
-  public editUser(index: number, editedUser: string) {
-    if (this.users[index]) {
-      this.users[index] = editedUser;
-    }
-  }
-  public get user() {
-    return this.users;
-  }
+  // public getUsersObs(): Observable<User[]> {
+  //   if (!this.users.length) {
+  //     return this.httpClient.get<User[]>('http://localhost:3000/users').pipe(
+  //       map((users: User[]) => {
+  //         this.users = users;
+  //         return users;
+  //       })
+  //     );
+  //   } else {
+  //     return of(this.users);
+  //   }
+  // }
 }
